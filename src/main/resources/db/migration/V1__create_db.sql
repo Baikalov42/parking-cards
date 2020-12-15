2e74f43100f3e2cbc426032c6d9e0384ba8335b8
Vladimir Baykalov <Baikalov.v42@yandex.ru>
2020-12-15 14:55:51 +0300

    sql style fix

--- src/main/resources/db/migration/V1__create_db.sql
+++ src/main/resources/db/migration/V1__create_db.sql
@@ -1,27 +1,43 @@
-create TABLE IF NOT EXISTS roles (id bigserial PRIMARY KEY, role text);
+create TABLE IF NOT EXISTS roles
+(
+    id   bigserial PRIMARY KEY,
+    role text
+);
 
 create TABLE IF NOT EXISTS users
-(id bigserial PRIMARY KEY,
-first_name text NOT NULL,
-last_name text NOT NULL,
-phone text,
-email text NOT NULL,
-role_id bigint,
-FOREIGN KEY (role_id) REFERENCES roles (id));
+(
+    id         bigserial PRIMARY KEY,
+    first_name text NOT NULL,
+    last_name  text NOT NULL,
+    phone      text,
+    email      text NOT NULL,
+    role_id    bigint,
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+);
 
-create TABLE IF NOT EXISTS car_brands (id bigserial PRIMARY KEY, name text, deleted boolean DEFAULT false);
+create TABLE IF NOT EXISTS car_brands
+(
+    id      bigserial PRIMARY KEY,
+    name    text,
+    deleted boolean DEFAULT false
+);
 
-create TABLE IF NOT EXISTS car_models (id bigserial PRIMARY KEY,
-  name text NOT NULL,
-  brand_id bigint,
-  deleted boolean DEFAULT false,
-  FOREIGN KEY (brand_id) REFERENCES car_brands (id));
+create TABLE IF NOT EXISTS car_models
+(
+    id       bigserial PRIMARY KEY,
+    name     text NOT NULL,
+    brand_id bigint,
+    deleted  boolean DEFAULT false,
+    FOREIGN KEY (brand_id) REFERENCES car_brands (id)
+);
 
 create TABLE IF NOT EXISTS cars
-(id bigserial PRIMARY KEY,
-model_id bigint,
-user_id bigint,
-license_plate text NOT NULL,
-FOREIGN KEY (model_id) REFERENCES car_models (id),
-FOREIGN KEY (user_id) REFERENCES users (id));
+(
+    id            bigserial PRIMARY KEY,
+    model_id      bigint,
+    user_id       bigint,
+    license_plate text NOT NULL,
+    FOREIGN KEY (model_id) REFERENCES car_models (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
 
--- src/main/resources/db/migration/V2__fill_brands_and_models.sql
+++ src/main/resources/db/migration/V2__fill_brands_and_models.sql
@@ -1,38 +1,37 @@
 INSERT INTO car_brands (name)
-VAlUES
-('Audi'),
-('Huyndai'),
-('Honda'),
-('BMW');
+VAlUES ('Audi'),
+       ('Huyndai'),
+       ('Honda'),
+       ('BMW');
 
 INSERT INTO car_models (name, brand_id)
-VALUES
-('A4', (select id from car_brands where name = 'Audi' limit 1)),
-('A5', (select id from car_brands where name = 'Audi' limit 1)),
-('Q3', (select id from car_brands where name = 'Audi' limit 1)),
-('Solaris', (select id from car_brands where name = 'Huyndai' limit 1)),
-('Creta', (select id from car_brands where name = 'Huyndai' limit 1)),
-('Civic', (select id from car_brands where name = 'Honda' limit 1)),
-('Accord', (select id from car_brands where name = 'Honda' limit 1)),
-('X3', (select id from car_brands where name = 'BMW' limit 1)),
-('X5', (select id from car_brands where name = 'BMW' limit 1)),
-('5', (select id from car_brands where name = 'BMW' limit 1)),
-('1', (select id from car_brands where name = 'BMW' limit 1));
+VALUES ('A4', (select id from car_brands where name = 'Audi' limit 1)),
+       ('A5', (select id from car_brands where name = 'Audi' limit 1)),
+       ('Q3', (select id from car_brands where name = 'Audi' limit 1)),
+       ('Solaris', (select id from car_brands where name = 'Huyndai' limit 1)),
+       ('Creta', (select id from car_brands where name = 'Huyndai' limit 1)),
+       ('Civic', (select id from car_brands where name = 'Honda' limit 1)),
+       ('Accord', (select id from car_brands where name = 'Honda' limit 1)),
+       ('X3', (select id from car_brands where name = 'BMW' limit 1)),
+       ('X5', (select id from car_brands where name = 'BMW' limit 1)),
+       ('5', (select id from car_brands where name = 'BMW' limit 1)),
+       ('1', (select id from car_brands where name = 'BMW' limit 1));
 
 INSERT INTO roles (role)
-VALUES
-('user'),
-('admin');
+VALUES ('user'),
+       ('admin');
 
 INSERT INTO users (first_name, last_name, email, phone, role_id)
-VALUES
-('Ivan', 'Ivanov', 'ivan@mail.ru', '+7921234567', (select id from roles where role = 'admin' limit 1)),
-('Petr', 'Petrov', 'petr@mail.ru', '+78124356709', (select id from roles where role = 'user' limit 1)),
-('Irina', 'Soboleva', 'ira@mail.ru', '+74352784903', (select id from roles where role = 'user' limit 1));
+VALUES ('Ivan', 'Ivanov', 'ivan@mail.ru', '+7921234567', (select id from roles where role = 'admin' limit 1)),
+       ('Petr', 'Petrov', 'petr@mail.ru', '+78124356709', (select id from roles where role = 'user' limit 1)),
+       ('Irina', 'Soboleva', 'ira@mail.ru', '+74352784903', (select id from roles where role = 'user' limit 1));
 
 INSERT INTO cars (model_id, user_id, license_plate)
-VALUES
-((select id from car_models where name = 'A4' limit 1), (select id from users where email = 'ivan@mail.ru' limit 1), 'A123BC178'),
-((select id from car_models where name = 'Solaris' limit 1), (select id from users where email = 'petr@mail.ru' limit 1), 'X342BH178'),
-((select id from car_models where name = 'X3' limit 1), (select id from users where email = 'ira@mail.ru' limit 1), 'T573TO178'),
-((select id from car_models where name = 'Q3' limit 1), (select id from users where email = 'ira@mail.ru' limit 1), 'O666OO178');
+VALUES ((select id from car_models where name = 'A4' limit 1),
+        (select id from users where email = 'ivan@mail.ru' limit 1), 'A123BC178'),
+       ((select id from car_models where name = 'Solaris' limit 1),
+        (select id from users where email = 'petr@mail.ru' limit 1), 'X342BH178'),
+       ((select id from car_models where name = 'X3' limit 1),
+        (select id from users where email = 'ira@mail.ru' limit 1), 'T573TO178'),
+       ((select id from car_models where name = 'Q3' limit 1),
+        (select id from users where email = 'ira@mail.ru' limit 1), 'O666OO178');
