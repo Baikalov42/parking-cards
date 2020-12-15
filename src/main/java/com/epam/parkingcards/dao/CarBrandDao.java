bdd50a2c44faa98e8ffedb500055338b4969351d
Romin Nuro <romin_nuro@epam.com>
2020-12-15 15:21:20 +0300

    CrudRepo changed to JpaRepo

--- src/main/java/com/epam/parkingcards/dao/CarBrandDao.java
+++ src/main/java/com/epam/parkingcards/dao/CarBrandDao.java
@@ -1,7 +1,8 @@
 package com.epam.parkingcards.dao;
 
 import com.epam.parkingcards.model.CarBrand;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CarBrandDao extends CrudRepository<CarBrand, Long> {
+public interface CarBrandDao extends JpaRepository<CarBrand, Long> {
 }
