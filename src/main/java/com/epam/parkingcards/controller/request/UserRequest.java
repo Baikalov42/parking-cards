b2b881f5a9c737eb6b429c9939b8a2ab2c95220e
Vladimir Baykalov <Baikalov.v42@yandex.ru>
2020-12-17 00:52:14 +0300

    add validation

--- src/main/java/com/epam/parkingcards/controller/request/UserRequest.java
+++ src/main/java/com/epam/parkingcards/controller/request/UserRequest.java
@@ -2,12 +2,28 @@ package com.epam.parkingcards.controller.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 public class UserRequest {
 
-    private long id;
+    private static final String NAME_PATTERN = "[A-Za-z]{2,29}";
+
+    @Pattern(regexp = NAME_PATTERN, message = "only latin letters")
+    @NotNull
+    @Size(min = 2, max = 30)
     private String firstName;
+
+    @Pattern(regexp = NAME_PATTERN)
+    @NotNull
+    @Size(min = 2, max = 30)
     private String lastName;
+
+    @Pattern(regexp = NAME_PATTERN)
+    @NotNull
+    @Size(min = 2, max = 30)
     private String phone;
     private String email;
     private String password;
