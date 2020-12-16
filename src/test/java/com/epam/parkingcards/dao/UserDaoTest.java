cf7b6ef6086d405fd56e7cf90447b6fcb639a7b2
Romin Nuro <romin_nuro@epam.com>
2020-12-17 01:43:09 +0300

    little fix

--- src/test/java/com/epam/parkingcards/dao/UserDaoTest.java
+++ src/test/java/com/epam/parkingcards/dao/UserDaoTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 import static org.assertj.core.api.Assertions.*;
 
