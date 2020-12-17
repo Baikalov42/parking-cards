129c8bf55ce50e5a02bcb601fdd60693effd67fc
Romin Nuro <romin_nuro@epam.com>
2020-12-17 17:52:09 +0300

    added query method and test

--- src/main/java/com/epam/parkingcards/dao/UserDao.java
+++ src/main/java/com/epam/parkingcards/dao/UserDao.java
@@ -2,10 +2,17 @@ package com.epam.parkingcards.dao;
 
 import com.epam.parkingcards.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserDao extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    @Query(nativeQuery = true,
+    value = "SELECT u.user_id, first_name, last_name, phone," +
+            " email, password FROM users as u JOIN cars as c " +
+            "on u.user_id = c.user_id where c.license_plate = :licensePlate")
+    Optional<User> findByLicensePlate(String licensePlate);
 }
--- src/test/java/com/epam/parkingcards/dao/UserDaoTest.java
+++ src/test/java/com/epam/parkingcards/dao/UserDaoTest.java
@@ -1,11 +1,14 @@
 package com.epam.parkingcards.dao;
 
+import com.epam.parkingcards.model.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -15,6 +18,13 @@ public class UserDaoTest {
 
     @Autowired
     private UserDao userDao;
+
+    @Test
+    public void given_userDao_when_findUserByPlate_then_returnValidUser() {
+        Optional<User> found = userDao.findByLicensePlate("A123BC178");
+        assertThat(found.isPresent());
+        assertThat(found.get().getFirstName()).isEqualTo("Ivan");
+    }
 
     @Test
     public void testFindAll() {
