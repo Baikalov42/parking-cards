4b20c60b2250ea86db52d24b568eeceaaa94f839
Romin Nuro <romin_nuro@epam.com>
2020-12-22 14:32:20 +0300

    New dao methods added

--- src/main/java/com/epam/parkingcards/dao/BrandDao.java
+++ src/main/java/com/epam/parkingcards/dao/BrandDao.java
@@ -4,14 +4,24 @@ import com.epam.parkingcards.model.BrandEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
-//TODO implement all methods with @Query annotation
 public interface BrandDao extends JpaRepository<BrandEntity, Long> {
-
+    @Query("SELECT b FROM BrandEntity b WHERE b.isDeleted = true")
     Page<BrandEntity> findAllDeleted(Pageable pageable);
 
-    //TODO вернуть количесво строк, где имя = brandName, и deleted = true
+    @Query("SELECT COUNT (b) FROM BrandEntity b WHERE b.name = :brandName AND b.isDeleted = true")
     Long getCountDeletedByName(String brandName);
 
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE BrandEntity b SET b.isDeleted = true WHERE b.id = :brandId")
     void markAsDeleted(long brandId);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE BrandEntity b SET b.isDeleted = false WHERE b.id = :brandId")
+    void restore(long brandId);
 }
