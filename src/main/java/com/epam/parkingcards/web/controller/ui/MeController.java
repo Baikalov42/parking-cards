0a954780bc3396a6f02e36533b173c9a43605c16
Vladimir Baykalov <Baikalov.v42@yandex.ru>
2021-01-11 00:59:07 +0300

    button "My Cars" available for authenticated users only, for pages user-create-car and user-edit-car field userId fill in hidden type. Refactor MeController method names

--- src/main/java/com/epam/parkingcards/web/controller/ui/MeController.java
+++ src/main/java/com/epam/parkingcards/web/controller/ui/MeController.java
@@ -21,9 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 import java.security.Principal;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/ui/me")
@@ -51,7 +49,7 @@ public class MeController {
     }
 
     @GetMapping("/cars")
-    public String getAllCars(Principal principal, Model model) {
+    public String getMyCars(Principal principal, Model model) {
         UserEntity me = userService.findByEmail(principal.getName());
         List<CarEntity> all = carService.findByUserId(me.getId());
         model.addAttribute("cars", all);
@@ -59,7 +57,7 @@ public class MeController {
     }
 
     @GetMapping("/update-page")
-    public String toUpdatePage(Principal principal, Model model) {
+    public String toUserUpdatePage(Principal principal, Model model) {
         UserEntity me = userService.findByEmail(principal.getName());
         model.addAttribute("me", me);
         model.addAttribute("userUpdateRequest", new UserUpdateRequest());
@@ -67,29 +65,26 @@ public class MeController {
     }
 
     @PostMapping("/edit")
-    public String update(@Valid UserUpdateRequest request, Model model) {
+    public String updateUser(@Valid UserUpdateRequest request, Model model) {
         UserEntity update = userService.update(userMapper.toUser(request));
         model.addAttribute("message", "ok");
         return MESSAGE_VIEW;
     }
 
     @GetMapping("/create-car")
-    public String toCreatePage(Principal principal, Model model) {
-        UserEntity me = userService.findByEmail(principal.getName());
+    public String toCarCreatePage(Principal principal, Model model) {
+        UserEntity user = userService.findByEmail(principal.getName());
 
-        Map<Long, String> usersMap = new HashMap<>();
-        usersMap.put(me.getId(),
-                String.format("%s %s", me.getFirstName(), me.getLastName()));
-
-        model.addAttribute("modelUsersMap", usersMap);
+        model.addAttribute("user", user);
         model.addAttribute("carCreateRequest", new CarCreateRequest());
         model.addAttribute("modelModelsMap", modelService.getModelsMap());
         model.addAttribute("modelBrandsMap", brandService.getBrandsMap());
+
         return "user/user-create-car";
     }
 
     @PostMapping("/car/create")
-    public String create(@Valid CarCreateRequest request, Model model) {
+    public String createCar(@Valid CarCreateRequest request, Model model) {
         long id = carService.create(carMapper.toCar(request));
         model.addAttribute("message", "ok, new id = " + id);
         return MESSAGE_VIEW;
@@ -97,32 +92,27 @@ public class MeController {
 
 
     @GetMapping("cars/update/{id}")
-    public String toUpdatePage(@PathVariable long id, Principal principal, Model model) {
-
-        UserEntity me = userService.findByEmail(principal.getName());
-        Map<Long, String> usersMap = new HashMap<>();
-        usersMap.put(me.getId(),
-                String.format("%s %s", me.getFirstName(), me.getLastName()));
+    public String toUserUpdatePage(@PathVariable long id, Principal principal, Model model) {
 
         CarEntity carEntity = carService.findById(id);
 
         model.addAttribute("carEntity", carEntity);
         model.addAttribute("carUpdateRequest", new CarUpdateRequest());
         model.addAttribute("modelModelsMap", modelService.getModelsMap());
-        model.addAttribute("modelUsersMap", usersMap);
         model.addAttribute("modelBrandsMap", brandService.getBrandsMap());
+
         return "user/cars-edit";
     }
 
     @PostMapping("cars/edit")
-    public String update(@Valid CarUpdateRequest request, Model model) {
+    public String updateCar(@Valid CarUpdateRequest request, Model model) {
         CarEntity updated = carService.update(carMapper.toCar(request));
         model.addAttribute("message", updated);
         return MESSAGE_VIEW;
     }
 
     @GetMapping("cars/delete/{id}")
-    public String delete(@PathVariable long id, Model model) {
+    public String deleteCar(@PathVariable long id, Model model) {
         carService.deleteById(id);
         model.addAttribute("message", id);
         return MESSAGE_VIEW;
