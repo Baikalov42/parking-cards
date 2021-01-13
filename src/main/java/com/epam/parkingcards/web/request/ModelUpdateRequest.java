b9e1203625af478ceb30365d6086b4ee98566f28
Vladimir Baikalov <vladimir_baikalov@epam.com>
2021-01-13 15:19:00 +0300

    validation Model and Brand fix

--- src/main/java/com/epam/parkingcards/web/request/ModelUpdateRequest.java
+++ src/main/java/com/epam/parkingcards/web/request/ModelUpdateRequest.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Size;
 @Data
 public class ModelUpdateRequest {
 
-    public static final String MODEL_NAME_PATTERN = "[A-Za-z0-9]{2,30}";
+    public static final String MODEL_NAME_PATTERN = "[A-Za-z0-9- ]{1,30}";
 
     @Min(value = 1, message = "Model id, should be greater than zero.")
     private long id;
@@ -20,7 +20,7 @@ public class ModelUpdateRequest {
 
     @NotEmpty(message = "Model name, must not be empty.")
     @Pattern(regexp = MODEL_NAME_PATTERN,
-            message = "Model name, must contains, only latin letters, numbers and '-'.")
-    @Size(min = 2, max = 30, message = "Model name, must 2 to 30 characters.")
+            message = "Model name, must contains only latin letters, numbers, spaces and '-'. ")
+    @Size(min = 1, max = 30, message = "Model name, must be 1 to 30 characters.")
     private String name;
 }
